a2c836a17c972d79bc626eaab987694c0ccf1f8c
Dirk Eddelbuettel <edd@debian.org>
2014-05-25 11:18:45 -0500

    added simple test (but protected by env.var)

---
+++ tests/simpleTests.R
@@ -0,0 +1,32 @@
+
+if (Sys.getenv("Run_RPushbullet_Tests")=="yes") {
+    ## These simple tests rely on the existence of a config file with key and device info
+    ## Otherwise we would have to hardcode a key and/or device id, and that is not something
+    ## one should store in a code repository.
+    ##
+    ## I put a request in to the authors of Pushbullet to provide a "test id" key, and they
+    ## are sympathetetic but to not have one yet.
+    stopifnot(file.exists("~/.rpushbullet.json"))
+
+    library(RPushbullet)
+
+    ## As there is a file, check the package global environment 'pb' we create on startup
+    RPushbullet:::.pkgglobalenv[["pb"]]
+    RPushbullet:::.pkgglobalenv[["pb"]][["key"]]
+    RPushbullet:::.pkgglobalenv[["pb"]][["devices"]]
+    
+    ## As well as the options we create then too
+    getOption("rpushbullet.key")
+    getOption("rpushbullet.devices")
+
+    ## Check the internal helper functions
+    RPushbullet:::.getKey()
+    RPushbullet:::.getDevices()
+
+    ## Show the list of devices registered to the key
+    require(RJSONIO)
+    str(fromJSON(pbGetDevices()))
+
+    ## Post a note item
+    str(fromJSON(pbPost("note", "simple Test", "We think this should work.\nWe really do.")))
+}
