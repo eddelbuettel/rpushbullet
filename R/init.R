a21374799bff81fd9756c7e0b2dc8a351d750f9c
Dirk Eddelbuettel <edd@debian.org>
2014-05-27 19:02:45 -0500

    added copyright headers

--- R/init.R
+++ R/init.R
@@ -1,4 +1,23 @@
 
+##  RPushbullet -- R interface to Pushbullet libraries
+##
+##  Copyright (C) 2014  Dirk Eddelbuettel <edd@debian.org>
+##
+##  This file is part of RPushbullet.
+##
+##  RPushbullet is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 2 of the License, or
+##  (at your option) any later version.
+##
+##  RPushbullet is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with RPushbullet.  If not, see <http://www.gnu.org/licenses/>.
+
 .pkgglobalenv <- new.env(parent=emptyenv())
 
 .onAttach <- function(libname, pkgname) {
