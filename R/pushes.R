dc84cf69be0008c9cafcb15b0fd1b716240f4f15
Bill Evans <BillEvans@ActiveDecisionSupport.com>
2014-06-06 09:14:17 -0700

    added option "rpushbullet.defdevice" to indicate the default device   index added .getDefDevice(), similar to .getDevices() updated pbPost() to handle vectors/lists of device indices as well as   default devices and all (pushbullet) devices

--- R/pushes.R
+++ R/pushes.R
@@ -32,13 +32,28 @@
 ##' browser is opened at the given URL.  If an \sQuote{address} is
 ##' pushed, the recipient's web browser is opened in map mode at the
 ##' given address.
+##'
+##' The \sQuote(deviceind} can be NULL to use the default device as
+##' defined as \sQuote{defdevice} in \code{~/.rpushbullet.json}; a
+##' positive integer to indicate the device to which the message will
+##' be sent; a vector or list of such indices; or 0 to send to all
+##' devices listed in the pushbullet account.
+##' 
+##' Note: when choosing 0 for the \sQuote{deviceind}, the message is
+##' sent to all devices in the pushbullet account, regardless of the
+##' \sQuote{devices} vector or those defined in
+##' \code{~/.rpushbullet.json}. If you want to send to only those
+##' defined in \sQuote{devices} excluding extras in your pushbullet
+##' account, provide a vector or list of device indices in
+##' \sQuote{deviceind}.
 ##' @title Post a message via Pushbullet
 ##' @param type The type of post: one of \sQuote{note}, sQuote{link}
 ##' or \sQuote{address}.
 ##' @param title The title of the note, or the name of the address, being posted.
 ##' @param body The body of the note, or the address when \code{type} is \sQuote{address}, or the (optional) body when the \code{type} is \sQuote{link}.
 ##' @param url The URL of \code{type} is \sQuote{link}.
-##' @param deviceind The index of the device in the list of devices, defaults to one.
+##' @param deviceind The index (or a vector/list of indices) of the
+##' device(s) in the list of devices. See 'Details'.
 ##' @param apikey The API key used to access the service. It can be
 ##' supplied as an argument here, via the global option
 ##' \code{rpushbullet.key}, or via the file \code{~/.rpushbullet.json}
@@ -56,7 +71,7 @@ pbPost <- function(type=c("note", "link", "address"), #"list", "file"),
                    body="",             # also address for type='address',
                                         # and items for type='list'
                    url="",
-                   deviceind=1,
+                   deviceind=NULL,
                    apikey = .getKey(),
                    devices = .getDevices(),
                    verbose = FALSE) {
@@ -65,43 +80,59 @@ pbPost <- function(type=c("note", "link", "address"), #"list", "file"),
     pburl <- "https://api.pushbullet.com/v2/pushes"
     curl <- .getCurl()
     
-    txt <- switch(type,
+    if (is.null(deviceind))
+        deviceind <- .getDefDevice()
 
-                  ## curl https://api.pushbullet.com/v2/pushes \
-                  ##   -u <your_api_key_here>: \
-                  ##   -d device_iden="<your_device_iden_here>" \
-                  ##   -d type="note" \
-                  ##   -d title="Note title" \
-                  ##   -d body="note body" \
-                  ##   -X POST
-                  note = sprintf(paste0('%s -s %s -u %s: -d device_iden="%s" ',
-                                        '-d type="note" -d title="%s" -d body="%s" -X POST'),
-                                 curl, pburl, apikey, devices[deviceind], title, body),
+    if (0 %in% deviceind) {
+        ## this will send to all devices in the pushbullet account,
+        ## explicitly including others would result in double-tapping
+        deviceind <- 0
+    }
 
-                  link = sprintf(paste0('%s -s %s -u %s: -d device_iden="%s" ',
-                                        '-d type="link" -d title="%s" -d body="%s" ',
-                                        '-d url="%s" -X POST'),
-                                 curl, pburl, apikey, devices[deviceind], title, body, url),
+    ret <- sapply(deviceind, function(ind) {
+        tgt <- ifelse(ind == 0,
+                      '',                             # all devices
+                      sprintf('-d device_iden="%s" ', # specific device
+                              devices[ind]))
 
-                  address = sprintf(paste0('%s -s %s -u %s: -d device_iden="%s" ',
-                                           '-d type="address" -d name="%s" -d address="%s" ',
-                                           '-X POST'),
-                                    curl, apikey, devices[deviceind], title, body)
+        txt <- switch(type,
 
-                  ## ## not quite sure what a list body would be
-                  ## list = sprintf(paste0('curl -s %s -u %s: -d device_iden="%s" ',
-                  ##                       '-d type="list" -d title="%s" -d items="%s" -X POST'),
-                  ##                pburl, apikey, device, title, body),
+                      ## curl https://api.pushbullet.com/v2/pushes \
+                      ##   -u <your_api_key_here>: \
+                      ##   -d device_iden="<your_device_iden_here>" \
+                      ##   -d type="note" \
+                      ##   -d title="Note title" \
+                      ##   -d body="note body" \
+                      ##   -X POST
+                      note = sprintf(paste0('%s -s %s -u %s: %s ',
+                          '-d type="note" -d title="%s" -d body="%s" -X POST'),
+                          curl, pburl, apikey, tgt, title, body),
 
-                  ## for file see docs, need to upload file first
-                  ## file = sprintf(paste0('curl -s %s -u %s: -d device_iden="%s" ',
-                  ##                       '-d type="link" -d title="%s" -d body="%s" ',
-                  ##                       '-d url="%s" -X POST'),
-                  ##                pburl, apikey, device, title, body, url),
+                      link = sprintf(paste0('%s -s %s -u %s: %s ',
+                          '-d type="link" -d title="%s" -d body="%s" ',
+                          '-d url="%s" -X POST'),
+                          curl, pburl, apikey, tgt, title, body, url),
 
-                  )
+                      address = sprintf(paste0('%s -s %s -u %s: %s ',
+                          '-d type="address" -d name="%s" -d address="%s" ',
+                          '-X POST'),
+                          curl, apikey, tgt, title, body)
 
-    if (verbose) print(txt)
-    res <- system(txt, intern=TRUE)
-    invisible(res)
+                      ## ## not quite sure what a list body would be
+                      ## list = sprintf(paste0('curl -s %s -u %s: -d device_iden="%s" ',
+                      ##                       '-d type="list" -d title="%s" -d items="%s" -X POST'),
+                      ##                pburl, apikey, device, title, body),
+
+                      ## for file see docs, need to upload file first
+                      ## file = sprintf(paste0('curl -s %s -u %s: -d device_iden="%s" ',
+                      ##                       '-d type="link" -d title="%s" -d body="%s" ',
+                      ##                       '-d url="%s" -X POST'),
+                      ##                pburl, apikey, device, title, body, url),
+
+                      )
+
+        if (verbose) print(txt)
+        system(txt, intern=TRUE)
+    })
+    invisible(ret)
 }
