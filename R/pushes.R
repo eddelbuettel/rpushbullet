c352bb2515be51afc2a68c1ea6975431a7be27bd
Dirk Eddelbuettel <edd@debian.org>
2020-01-12 10:17:20 -0600

    make HTTP 1.1 use optional

--- R/pushes.R
+++ R/pushes.R
@@ -4,7 +4,7 @@
 ##  Copyright (C) 2014         Dirk Eddelbuettel <edd@debian.org>
 ##  Copyright (C) 2014 - 2016  Dirk Eddelbuettel and Mike Birdgeneau
 ##  Copyright (C) 2014 - 2019  Dirk Eddelbuettel, Mike Birdgeneau and Seth Wenchel
-##  Copyright (C) 2019 -       Dirk Eddelbuettel, Mike Birdgeneau, Seth Wenchel
+##  Copyright (C) 2019 - 2020  Dirk Eddelbuettel, Mike Birdgeneau, Seth Wenchel
 ##                             and Chan-Yub Park
 ##
 ##  This file is part of RPushbullet.
@@ -52,6 +52,12 @@
 ##'
 ##' The earlier argument \code{deviceind} is now deprecated and will
 ##' be removed in a later release.
+##'
+##' In some cases servers may prefer the older \sQuote{HTTP 1.1}
+##' standard (as opposed to the newer \sQuote{HTTP 2.0} set by
+##' \code{curl}). Setting the option \dQuote{rpushbullet.useHTTP11} to
+##' \code{TRUE} will enable use of \sQuote{HTTP 1.1}.
+##'
 ##' @title Post a message via Pushbullet
 ##' @param type The type of post: one of \sQuote{note}, \sQuote{link}, or \sQuote{file}.
 ##' @param title The title of the note being posted.
